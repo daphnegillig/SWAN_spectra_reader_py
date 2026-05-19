57d3c29d91a878db0e5055cee6f0a63ebb215a72
daphne_seaphysics <daphnegillig@gmail.com>
2026-05-19 15:12:31 +0200

    Enhance spec1dread for non-stationary data handling
    Modified the spec1dread function to handle non-stationary data and added a print statement for start time.

--- loadspec_1D.py
+++ loadspec_1D.py
@@ -2,6 +2,8 @@
 """
 Created on Mon May  4 15:52:27 2026
 
+Modified on Mon May  19 15:11:56 2026
+
 @author: daphne_seaphysics
 """
 
@@ -26,12 +28,15 @@ def spec1dread(filename):
     #checking for non-stationary data
     if index<len(lines) and lines[index].startswith('TIME'):
         time=1
-        #check to increases index
-    #get number of locations
-    if index<len(lines) and lines[index].startswith('LOCATION'):
-        index +=1
-        nloc = int(lines[index].split()[0])
+    if time==1:
+        index+=3 
+        nloc=int(lines[index].split()[0])
         index+=1 
+    else:     
+        if index<len(lines) and lines[index].startswith('LOCATION'):
+            index +=1
+            nloc = int(lines[index].split()[0])
+            index+=1 
     #append locations to x and y arrays
     for _ in range(nloc):
         if index<len(lines):
@@ -39,23 +44,20 @@ def spec1dread(filename):
             x.append(coords[0])
             y.append(coords[1])
             index +=1
-
     if index<len(lines) and lines[index].startswith('AFREQ'):
         index+=1 
         nfreq = int(lines[index].split()[0])
         index+=1
-    for _ in range(nfreq):
-        f = lines[index]
-        f=float(f)
-        freq.append(f)
-        index+=1
-    
+        for _ in range(nfreq):
+            f = lines[index]
+            f=float(f)
+            freq.append(f)
+            index+=1
     #reading spectra
     if index<len(lines) and lines[index].startswith('QUANT'):
         index+=1 
         quant=int(lines[index].split()[0])
-        index+=1
-        
+        index+=1 
     index+=3
     if index<len(lines) and lines[index].startswith('CDIR'):
         print(lines[index])
@@ -84,5 +86,31 @@ def spec1dread(filename):
                 
                 S.append({'f':freq,'S':spec,'dir':dire,'spread':spr, 'pos':[x[ii],y[ii]],'type':'freq'})
                 index-=1 
+    elif time==1:
+        index+=1
+        print('start time:', lines[index].split()[0])
+        index+=1 
+        for ii in range(nloc):
+            if index>=len(lines):
+                break
+            index+=1
+            if index<len(lines) and lines[index].startswith('NODATA'):
+                index+=1
+            if index<len(lines) and lines[index].startswith('LOCATI'):
+                index+=1 
+                spec=[]
+                dire=[]
+                spr=[]
+                for j in range(nfreq):
+                    if index >= len(lines):
+                        break
+                    spec_line = lines[index].split()
+                    spec_line[0], spec_line[1], spec_line[2] = float(spec_line[0]), float(spec_line[1]), float(spec_line[2])
+                    spec.append(spec_line[0])
+                    dire.append(spec_line[1])
+                    spr.append(spec_line[2])
+                    index+=1
                 
+                S.append({'f':freq,'S':spec,'dir':dire,'spread':spr, 'pos':[x[ii],y[ii]],'type':'freq'})
+                index-=1 
     return S
